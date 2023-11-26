8138d10d1145f91772a5a59137b63a5101294053
TakumiY08 <yonemurat@student.douglascollege.ca>
2023-11-25 18:56:13 -0800

    add constList

--- TechAssist/src/main/java/com/example/techassist/Utilities/ConstList.java
+++ TechAssist/src/main/java/com/example/techassist/Utilities/ConstList.java
@@ -6,6 +6,17 @@ import org.springframework.stereotype.Component;
 public class ConstList {
     public final static String KEY_ID = "id";
     public final static String KEY_USER_NAME = "userName";
+    public final static String KEY_PASSWORD = "password";
+    public final static String KEY_CLIENT_ID = "clientId";
+    public final static String KEY_TECHNICIAN_ID = "technicianId";
+    public final static String KEY_APPOINTMENT_DATE = "appointmentDATE";
+    public final static String KEY_SELECTED_FIELD = "selectedField";
+    public final static String KEY_TECHNICIAN_LIST = "technicianList";
+    public final static String FORMAT_DATE = "yyyy-MM-dd";
+    public final static String FORMAT_DATE_TIME = "MM/dd/yyyy HH:mm:ss";
+    public final static String KEY_CALL_ID = "callId";
+    public final static String KEY_TIME_SLOT_LIST = "timeSlotList";
+
     public final static String KEY_CLIENT = "client";
     public final static String KEY_TECHNICIAN = "technician";
     public final static String KEY_SERVICE_FIELD_LIST = "serviceFieldList";
